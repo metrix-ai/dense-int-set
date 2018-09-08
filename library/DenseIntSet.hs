d31d69025aa4f85f8a7364972ab35bcaa71f4f13
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-09-08 16:22:04 +0300

    Init

---
+++ library/DenseIntSet.hs
@@ -0,0 +1,48 @@
+module DenseIntSet
+where
+
+import DenseIntSet.Prelude
+import qualified Data.Vector.Unboxed as UnboxedVector
+import qualified Data.Vector.Unboxed.Bit as BitVec
+
+
+-- * IntSet
+-------------------------
+
+{-|
+Since there's multiple ways to implement a monoid for this data-structure,
+the instances are provided for the specific wrappers like "IntSetIntersection" or "IntSetUnion".
+All of those types are interconvertible.
+-}
+newtype IntSet = IntSet BitVector
+
+intersection :: IntSetIntersection -> IntSet
+intersection (IntSetIntersection minLength vecs) = IntSet (BitVec.intersections minLength vecs)
+
+union :: IntSetUnion -> IntSet
+union (IntSetUnion minLength vecs) = IntSet (BitVec.unions minLength vecs)
+
+
+-- * Intersection composition
+-------------------------
+
+data IntSetIntersection = IntSetIntersection !Int [BitVector]
+
+instance Semigroup IntSetIntersection where
+  (<>) (IntSetIntersection leftMinLength leftVecs) (IntSetIntersection rightMinLength rightVecs) =
+    IntSetIntersection (min leftMinLength rightMinLength) (leftVecs <> rightVecs)
+
+instance Monoid IntSetIntersection where
+  mempty = IntSetIntersection 0 []
+  mappend = (<>)
+
+intersected :: IntSet -> IntSetIntersection
+intersected (IntSet vec) = IntSetIntersection (BitVec.length vec) (pure vec)
+
+
+-- * Union composition
+-------------------------
+
+data IntSetUnion = IntSetUnion !Int [BitVector]
+
+
